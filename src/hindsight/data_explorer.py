68bc1e66fdf59b0e319d5f6f279b89e18e092f7d
Srijan Dhungana <srijan.dhungana6@gmail.com>
2025-11-12 00:10:49 +0200

    add more technicals

--- src/hindsight/data_explorer.py
+++ src/hindsight/data_explorer.py
@@ -9,6 +9,7 @@ class CandleSource(Enum):
     High  = 1
     Low   = 2
     Close = 3
+    Volume = 4
 
 class DataExplorer:
     def __init__(self, candles: list[Candle]):
@@ -17,18 +18,11 @@ class DataExplorer:
             CandleSource.Open: np.fromiter((candle.open for candle in self.candles), dtype=np.float64),
             CandleSource.High: np.fromiter((candle.high for candle in self.candles), dtype=np.float64),
             CandleSource.Low: np.fromiter((candle.low for candle in self.candles), dtype=np.float64),
-            CandleSource.Close: np.fromiter((candle.close for candle in self.candles), dtype=np.float64)
+            CandleSource.Close: np.fromiter((candle.close for candle in self.candles), dtype=np.float64),
+            CandleSource.Volume: np.fromiter((candle.volume for candle in self.candles), dtype=np.float64),
         }
         self.techs : dict = {}
         pass
-
-    def get_local_explorer(self, index: int) -> LocalDataExplorer:
-        return LocalDataExplorer(self, index)
-
-    def get_candle_at(self, index: int) -> Candle | None:
-        if(index < 0 or index >= len(self.candles)):
-            return None
-        return self.candles[index]
 
     def has_tech(self,  type: str, props: dict) -> bool:
         if(type in self.techs.keys()):
@@ -38,6 +32,29 @@ class DataExplorer:
                     return True
         else:
             False  
+
+    def ta_calculate(self, type: str, props: dict) -> np.ndarray:
+        if(type == "SMA"):
+            source : CandleSource = props["source"]
+            return ta.SMA(self.ohlc[source], props["period"])
+        elif(type == "EMA"):
+            source : CandleSource = props["source"]
+            return ta.EMA(self.ohlc[source], props["period"])
+        elif(type == "ATR"):
+            return ta.ATR(self.ohlc[CandleSource.High], self.ohlc[CandleSource.Low], self.ohlc[CandleSource.Close], props["period"])
+        
+        return np.ndarray([])
+
+    def get_local_explorer(self, index: int) -> LocalDataExplorer:
+        return LocalDataExplorer(self, index)
+
+    def get_candle_at(self, index: int) -> Candle | None:
+        if(index < 0 or index >= len(self.candles)):
+            return None
+        return self.candles[index]
+
+    def get_candle_data(self, source: CandleSource) -> np.ndarray:
+        return self.ohlc[source]
 
     def get_tech(self, index: int, type: str, props: dict) -> float:
         if(type in self.techs.keys()):
@@ -70,20 +87,22 @@ class DataExplorer:
             "value": data
         })
         return data
+    
 
-    def get_ma(self, period: int, source : CandleSource = CandleSource.Close) -> np.ndarray:
+    def get_tech_sma(self, period: int, source : CandleSource = CandleSource.Close) -> np.ndarray:
         return self.get_tech_full("SMA", {
             "period": period,
             "source" : source
         })
-
-
-    def ta_calculate(self, type: str, props: dict) -> np.ndarray:
-        source : CandleSource = props["source"]
-        if(type == "SMA"):
-            return ta.SMA(self.ohlc[source], props["period"])
-        
-        return np.ndarray([])
+    def get_tech_ema(self, period: int, source : CandleSource = CandleSource.Close) -> np.ndarray:
+        return self.get_tech_full("EMA", {
+            "period": period,
+            "source" : source
+        })
+    def get_tech_atr(self, period: int) -> np.ndarray:
+        return self.get_tech_full("ATR", {
+            "period": period
+        })
 
 class LocalDataExplorer:
     def __init__(self, parent : DataExplorer, index: int):
@@ -97,4 +116,13 @@ class LocalDataExplorer:
         return self.parent.get_tech(self.index, "SMA", {
             "period": period,
             "source" : source
+        })
+    def get_tech_ema(self, period: int, source : CandleSource = CandleSource.Close) -> float:
+        return self.parent.get_tech(self.index, "EMA", {
+            "period": period,
+            "source" : source
+        })
+    def get_tech_atr(self, period: int) -> float:
+        return self.parent.get_tech(self.index, "ATR", {
+            "period": period
         })
--- src/hindsight/filter.py
+++ src/hindsight/filter.py
@@ -4,6 +4,9 @@ import numpy as np
 
 class Filter(ABC):
     @abstractmethod
+    def prepare(self, explorer: DataExplorer) -> None:
+        ...
+    @abstractmethod
     def run(self, explorer: LocalDataExplorer) -> bool:
         ...
 
@@ -13,12 +16,12 @@ class FilterPipeline:
         self.filters : list[Filter]= filters
         self.candle_mask : np.ndarray[bool] = []
 
-
     def run(self, explorer: DataExplorer) -> None:
         candle_count = len(explorer.candles)
         self.candle_mask : np.ndarray = np.ones(candle_count, dtype=bool)
-        
+
         for filter in self.filters:
+            filter.prepare(explorer)
             for ci in range(candle_count):
                 if(self.candle_mask[ci]):
                     self.candle_mask[ci] = filter.run(explorer.get_local_explorer(ci))
