d46a98d30c74191a2d91c6448a53f42af361b176
Srijan Dhungana <srijan.dhungana6@gmail.com>
2025-11-13 21:09:35 +0200

    Print out filtering statistics

--- src/hindsight/filter.py
+++ src/hindsight/filter.py
@@ -17,13 +17,27 @@ class FilterPipeline:
         self.candle_mask : np.ndarray[bool] = []
 
     def run(self, explorer: DataExplorer) -> None:
-        candle_count = len(explorer.candles)
-        self.candle_mask : np.ndarray = np.ones(candle_count, dtype=bool)
+        total_candles = len(explorer.candles)
+        self.candle_mask : np.ndarray = np.ones(total_candles, dtype=bool)
+
+        print(f"Processing {len(explorer.candles)} candles")
+        print("-" * 20)
 
         for filter in self.filters:
             filter.prepare(explorer)
-            for ci in range(candle_count):
+            discard_count = 0
+            passed_count = 0
+            for ci in range(total_candles):
                 if(self.candle_mask[ci]):
                     self.candle_mask[ci] = filter.run(explorer.get_local_explorer(ci))
+                    if(self.candle_mask[ci]):
+                        passed_count += 1
+                    else:
+                        discard_count += 1
 
-        
+            # Print out how many candles the filter passed out of all candles
+            filter_name = filter.__class__.__name__
+            print(f"Filter: {filter_name}")
+            print(f"Candles Discarded: {discard_count}")
+            print(f"Candles Passed: {passed_count}")
+            print("-" * 20)
